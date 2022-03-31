bf4123f1a8a25f169800f585a3278ab5197371cb
Kursat_Tezgel <kursattezgel@hotmail.com>
2022-03-31 16:44:59 +0300

    PageObjects added

--- src/test/java/Basics.java
+++ src/test/java/Basics.java
@@ -1,5 +1,8 @@
+import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import pageObjects.HomePage;
+import pageObjects.Preferences;
 
 
 import java.net.MalformedURLException;
@@ -11,11 +14,20 @@ public class Basics extends Base{
         AndroidDriver<AndroidElement> driver = capabilities("real");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
-        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
-        driver.findElementByXPath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]").click();
+        //driver.findElementByXPath("//android.widget.TextView[@content-desc=\"Preference\"]").click();
+        HomePage homePage = new HomePage(driver);
+        homePage.Preferences.click();
+
+        Preferences preferences = new Preferences(driver);
+        preferences.PreferenceDependencies.click();
+        //driver.findElementByXPath("//android.widget.TextView[@content-desc=\"3. Preference dependencies\"]").click();
+
         driver.findElementById("android:id/checkbox").click();
         driver.findElementsByClassName("android.widget.RelativeLayout").get(1).click();
         driver.findElementById("android:id/edit").sendKeys("Hello Guys");
-        driver.findElementById("android:id/button1").click();
+        //driver.findElementById("android:id/button1").click();
+        //driver.findElementsByClassName("android.widget.Button").get(1).click();
+
+        preferences.Buttons.get(1).click();
     }
 }
---
+++ src/test/java/pageObjects/HomePage.java
@@ -0,0 +1,18 @@
+package pageObjects;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage {
+    public HomePage(AppiumDriver driver) {
+        PageFactory.initElements(new AppiumFieldDecorator(driver), this);
+    }
+    @AndroidFindBy(xpath = "//android.widget.TextView[@content-desc=\"Preference\"]")
+    public WebElement Preferences;
+
+}
