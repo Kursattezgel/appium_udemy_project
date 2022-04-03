16064e929a78f3d2fdb021614b201a586553fb81
Kursat_Tezgel <kursattezgel@hotmail.com>
2022-04-03 17:28:54 +0300

    Parameterization in TestNG xml file

--- src/test/java/TestNGTutorial/LoginCar.java
+++ src/test/java/TestNGTutorial/LoginCar.java
@@ -2,7 +2,7 @@ package TestNGTutorial;
 
 import org.testng.annotations.*;
 
-import java.sql.SQLOutput;
+import java.util.Iterator;
 
 public class LoginCar {
     @BeforeClass
@@ -13,12 +13,13 @@ public class LoginCar {
     public void afterclass(){
         System.out.println("after executing all methods in the class");
     }
-    @Parameters({"URL"})
+    @Parameters({"URL","APIKey/username"})
     @Test
-    public void WebLoginCarLoan(String urlName){
+    public void WebLoginCarLoan(String urlName, String key){
         //selenium
         System.out.println("weblogincar");
         System.out.println(urlName);
+        System.out.println(key);
     }
     @BeforeMethod
     public void beforeevery(){
@@ -42,18 +43,43 @@ public class LoginCar {
     @Test(enabled = false) //(timeOut = 4000)
     public void MobileSigninCarLoan() {
         //appium
-        System.out.println("MobileSigninCarLoan");
+        System.out.println("MobileSigninCarLoan");;
     }
 
-    @Test(timeOut = 4000) //(enabled = false)
-    public void MobileSignoutCarLoan() {
+    @Test(dataProvider = "getData") //(enabled = false) (timeOut = 4000)
+    public void MobileSignoutCarLoan(String username, String password) {
         //appium
         System.out.println("MobileSignoutCarLoan");
+        System.out.println(username);
+        System.out.println(password);
     }
 
     @Test(dependsOnMethods ={"WebLoginCarLoan","MobileSignoutCarLoan"} )
     public void APICarLoan(){
         //Rest API automation
         System.out.println("APIlogincar");
+    }
+
+    @DataProvider
+    public Object[][] getData(){
+        //1st combination - username password - good credit history= row
+        //2nd - username password  - no credit history
+        //3rd - fraudulent credit history
+        Object[][] data = new Object[3][2];
+        //1st set
+        data[0][0] = "firstsetusername";
+        data[0][1] = "firstpassword";
+        //columns in the row are nothing but values for that particular combination(row)
+
+        //2nd set
+        data[1][0] = "secondsetusername";
+        data[1][1] = "secondpassword";
+
+        //3rd set
+        data[2][0] = "thirdsetusername";
+        data[2][1] = "thirdpassword";
+
+        return data;
+
     }
 }
